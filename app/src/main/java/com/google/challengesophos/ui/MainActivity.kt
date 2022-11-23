f8d85f902fe41581475522aca18a8b5a3e9a3c28
Pablo Beltran <arturo3393@gmail.com>
2022-11-23 15:17:06 -0500

    Postdocuments and getOffice added, but they still need to get the post confirmation of the document and the info of the cities. They are just sending the request

--- app/src/main/java/com/google/challengesophos/ui/MainActivity.kt
+++ app/src/main/java/com/google/challengesophos/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.google.challengesophos.ui
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,7 +9,9 @@ import androidx.activity.viewModels
 import com.google.challengesophos.R
 import com.google.challengesophos.ViewModel.GetDocsByIdViewModel
 import com.google.challengesophos.ViewModel.GetDocsViewModel
+import com.google.challengesophos.ViewModel.GetOfficesViewModel
 import com.google.challengesophos.ViewModel.LoginViewModel
+import org.w3c.dom.Text
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,8 +21,11 @@ class MainActivity : AppCompatActivity() {
 
     //private val getDocsViewModel:GetDocsViewModel by viewModels()
 
-    private val getDocsByIdViewModel: GetDocsByIdViewModel by viewModels()
+    //private val getDocsByIdViewModel: GetDocsByIdViewModel by viewModels()
 
+    private val getOfficesViewModel: GetOfficesViewModel by viewModels()
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,11 +33,11 @@ class MainActivity : AppCompatActivity() {
         val logInBtn: Button = findViewById(R.id.btnLogin)
         val emailIn: TextView = findViewById(R.id.etEmail)
         val passwordIn: TextView = findViewById(R.id.etPassword)
-        val docSpecificId: TextView =findViewById(R.id.tvIdDocument)
-
+        val docSpecificId: TextView = findViewById(R.id.tvIdDocument)
+        val cityIn: TextView = findViewById(R.id.tvCity)
 
 //listens and saves the email and password from the login
-     /*   logInBtn.setOnClickListener {
+        /*   logInBtn.setOnClickListener {
 
             val emailLogin = emailIn.text.toString().trim()
             val passwordLogin = passwordIn.text.toString().trim()
@@ -41,17 +47,22 @@ class MainActivity : AppCompatActivity() {
 
 // Listens and brings the list of docs only with the email and pressing the btn
 
-     /*   logInBtn.setOnClickListener {
+        /*   logInBtn.setOnClickListener {
             val emailLogin = emailIn.text.toString().trim()
             getDocsViewModel.getDocsViewModel(emailLogin)
         }*/
 
 
-        logInBtn.setOnClickListener {
+// Listens and brings all the detail info of a documents by its ID
+        /*  logInBtn.setOnClickListener {
             val idDoc = docSpecificId.text.toString().trim()
             getDocsByIdViewModel.getDocsViewModel(idDoc)
-        }
+        }*/
 
+        logInBtn.setOnClickListener {
+            val city = cityIn.text.toString().trim()
+            getOfficesViewModel.getOfficesViewModel(city)
+        }
 
 
     }
