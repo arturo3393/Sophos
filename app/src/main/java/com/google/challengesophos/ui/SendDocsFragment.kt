57f388e03e492f882ce6d9556c91dc416bb7e3bf
Pablo Beltran <arturo3393@gmail.com>
2022-12-02 16:02:48 -0500

    Permissions for use of camera and storage added

--- app/src/main/java/com/google/challengesophos/ui/SendDocsFragment.kt
+++ app/src/main/java/com/google/challengesophos/ui/SendDocsFragment.kt
@@ -1,5 +1,6 @@
 package com.google.challengesophos.ui
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,7 +8,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import com.google.challengesophos.R
 import com.google.challengesophos.ViewModel.PostDocViewModel
@@ -25,6 +28,12 @@ class SendDocsFragment : Fragment(R.layout.fragment_send_docs), AdapterView.OnIt
 
     //Adapter fot the spiner of cities
     lateinit var arrayAdapterCities: ArrayAdapter<String>
+
+    //The code of the permission for the camera
+    private val PERMISSION_CAMARA:Int = 100
+
+    //The code of the permission for the storage
+    private val PERMISSION_EXTERNAL_STORAGE: Int = 100
 
 
     // This property is only valid between onCreateView and
@@ -92,12 +101,26 @@ class SendDocsFragment : Fragment(R.layout.fragment_send_docs), AdapterView.OnIt
 
         binding.etEmailSendDocs.setText(arguments?.getString("user_email"))
 
+        binding.ivTakePhotoDocs.setOnClickListener {
+            askForCameraPermission()
+        }
+
+        binding.btnAttachDoc.setOnClickListener {
+            askForFilesPermission()
+
+            Toast.makeText(
+                context,
+                "TWEEEE.",
+                Toast.LENGTH_SHORT
+            ).show()
+
+        }
 
 
         return binding.root
 
 
-        //05ftK5Ly0J9s
+
     }
 
 
@@ -114,11 +137,89 @@ class SendDocsFragment : Fragment(R.layout.fragment_send_docs), AdapterView.OnIt
         TODO("Not yet implemented")
     }
 
+    //1st the app already has permission, 2. Permission was denied previously. 3. First time the app ask for permission
+    private fun askForCameraPermission() {
+        when {
+            ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_GRANTED -> {
+                takePhoto()
+            }
+            shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA) -> {
+                Toast.makeText(
+                    context,
+                    "The permission was previously denied, allow it in settings.",
+                    Toast.LENGTH_SHORT
+                ).show()
 
-    /*
-    //Saves the value of the live data to make it shorter in the adapter
-    private fun getCitiesIndex(index: Int): String {
-        return postDocViewModel.citiesLiveData.value?.get(index)?.toString() ?: ""
-    }*/
+            }
+            else -> {
+                requestPermissions(arrayOf(android.Manifest.permission.CAMERA), PERMISSION_CAMARA)
+            }
+
+        }
+    }
+
+    private fun askForFilesPermission(){
+        when {
+            ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED -> {
+                uploadPhoto()
+            }
+            shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                Toast.makeText(
+                    context,
+                    "The permission was previously denied, allow it in settings.",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
+            else -> {
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSION_EXTERNAL_STORAGE)
+            }
+
+        }
+
+
+    }
+
+    //allows to evaluate the result of the permission
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            PERMISSION_CAMARA -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        takePhoto()
+                }
+            }
+
+            PERMISSION_EXTERNAL_STORAGE-> {
+                if (grantResults.isNotEmpty() && grantResults[1] == PackageManager.PERMISSION_GRANTED)
+                    uploadPhoto()
+            }
+            else-> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        }
+
+    }
+
+    private fun takePhoto() {
+
+    }
+
+    private fun uploadPhoto(){
+
+    }
+
 
 }
+
+
+/*
+//Saves the value of the live data to make it shorter in the adapter
+private fun getCitiesIndex(index: Int): String {
+    return postDocViewModel.citiesLiveData.value?.get(index)?.toString() ?: ""
+}*/
+
