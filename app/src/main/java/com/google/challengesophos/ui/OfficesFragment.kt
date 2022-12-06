9e455ca0b933b091ef8eac5e402c5abfde75a256
Pablo Beltran <arturo3393@gmail.com>
2022-12-06 17:01:51 -0500

    location permission added and cellphone location

--- app/src/main/java/com/google/challengesophos/ui/OfficesFragment.kt
+++ app/src/main/java/com/google/challengesophos/ui/OfficesFragment.kt
@@ -1,25 +1,30 @@
 package com.google.challengesophos.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.challengesophos.R
 import com.google.challengesophos.databinding.FragmentOfficesBinding
 
 class OfficesFragment : Fragment(), OnMapReadyCallback {
 
-    private lateinit var map:GoogleMap
+    private val REQUEST_CODE_LOCATION = 0
+
+    private lateinit var map: GoogleMap
 
     private var _binding: FragmentOfficesBinding? = null
 
@@ -42,7 +47,7 @@ class OfficesFragment : Fragment(), OnMapReadyCallback {
 
 
         //puts the name to the appbar
-        (activity as AppCompatActivity).supportActionBar?.title ="Regresar"
+        (activity as AppCompatActivity).supportActionBar?.title = "Regresar"
         //Sets the back arrow and the icon for it
         (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
         (activity as AppCompatActivity).supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back_arrow_light)
@@ -63,16 +68,67 @@ class OfficesFragment : Fragment(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
         createMarker()
+        enableLocation()
     }
 
-    private fun createMarker (){
-        val coordinates = LatLng()
-        var marker: MarkerOptions = MarkerOptions().position(coordinates).title()
+    private fun createMarker() {
+        val coordinates = LatLng(4.6796679999999995, -74.044757)
+        var marker: MarkerOptions =
+            MarkerOptions().position(coordinates).title("Edificio Davivienda - Piso 4")
         map.addMarker(marker)
         map.animateCamera(
-            CameraUpdateFactory.newLatLngZoom(coordinates,18f), 4000, null
+            CameraUpdateFactory.newLatLngZoom(coordinates, 18f), 4000, null
         )
     }
 
+    private fun isLocationPermissionGranted() = ContextCompat.checkSelfPermission(
+        requireContext(),
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private fun enableLocation() {
+        if (!::map.isInitialized) return
+        if (isLocationPermissionGranted()) {
+            map.isMyLocationEnabled = true
+        } else {
+            requestLocationPermission()
+        }
+
+    }
+
+    private fun requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
+            Toast.makeText(context, "Set up location permissions in settings", Toast.LENGTH_SHORT)
+                .show()
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_CODE_LOCATION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_CODE_LOCATION -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                map.isMyLocationEnabled = true
+            } else {
+                Toast.makeText(
+                    context,
+                    "Set up location permissions in settings",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
 
 }
